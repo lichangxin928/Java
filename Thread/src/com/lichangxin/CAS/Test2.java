89ae014839a2a08f9bf1730af91cd082477924cc
lichangxin <2431424266@qq.com>
2021-09-03 19:34:59 +0800

    Atomics

--- Thread/src/com/lichangxin/CAS/Test2.java
+++ Thread/src/com/lichangxin/CAS/Test2.java
@@ -1,7 +1,10 @@
 package com.lichangxin.CAS;
 
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicLong;
+
 public class Test2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         /*
         * 原子类：
         * 基础数据型：AtomicInteger、AtomicLong、AtomicBoolean
@@ -12,6 +15,64 @@ public class Test2 {
         *
         * 使用AtomicLong计数器
         * */
-        
+
+        // 模拟用户请求
+        for (int i = 0;i<=10000;i++){
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    Indicator.getINDICATOR().newRequestReceive();
+                    int num = new Random().nextInt();
+                    if(num % 2 == 0 ){
+                        Indicator.getINDICATOR().successReuest();
+                    }else{
+                        Indicator.getINDICATOR().failureRequest();
+                    }
+                }
+            }).start();
+        }
+        Thread.sleep(1000);
+        System.out.println(Indicator.getINDICATOR().getRequestCount());
+        System.out.println(Indicator.getINDICATOR().getSuccessCount());
+        System.out.println(Indicator.getINDICATOR().getFailureCount());
+
+    }
+}
+class Indicator{
+    // 单例
+    private Indicator(){};
+    // 定义一个私有的本类静态对象
+    private static final Indicator INDICATOR = new Indicator();
+    // 提供一个公共静态方法返回唯一实例
+
+    public static Indicator getINDICATOR() {
+        return INDICATOR;
+    }
+    private final AtomicLong requestCount = new AtomicLong(0);
+    private final AtomicLong successCount = new AtomicLong(0);
+    private final AtomicLong failureCount = new AtomicLong(0);
+    // 有新的请求
+    public void newRequestReceive(){
+        requestCount.incrementAndGet();
+    }
+    // 成功数
+    public void successReuest(){
+        successCount.incrementAndGet();
+    }
+    // 失败数
+    public void failureRequest (){
+        failureCount.incrementAndGet();
+    }
+
+    public long getFailureCount() {
+        return failureCount.get();
+    }
+
+    public long getRequestCount() {
+        return requestCount.get();
+    }
+
+    public long getSuccessCount() {
+        return successCount.get();
     }
 }
