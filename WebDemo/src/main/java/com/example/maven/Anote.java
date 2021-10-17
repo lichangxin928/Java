bf0276ad50fb7e79fb9614bbcdeaa6390dc0f38e
lichangxin928 <2431424266@qq.com>
2021-10-17 12:42:53 +0800

    maven

---
+++ WebDemo/src/main/java/com/example/maven/Anote.java
@@ -0,0 +1,21 @@
+package com.example.maven;
+
+public class Anote {
+    /**
+     * Maven 常用命令
+     *  compile ：编译
+     *  clear ： 清理 target 目录
+     *  package ： 打包
+     *  install ： 将项目安装到本地仓库
+     * Maven 生命周期
+     *  CLean 在进行构建之前进行一些清理工作
+     *  Default 构建的核心部分 编译、测试、打包、部署等等
+     *  Site 生成项目报告、站点、发布站点
+     * Maven 坐标
+     *  被 Maven 管理的资源的唯一标识
+     *      groupId：组织名称
+     *      artifactId：模块名称
+     *      version：版本号
+     *
+     */
+}
